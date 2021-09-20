a671ce26329dd79420cc4bd2de2af82da02e611d
Mayu008 <kadam.ms@somaiya.edu>
2021-09-21 00:42:46 +0530

    dp-21/9/2021

--- Dynamic programming/MaximumLengthOfSubarrayWithPositiveProduct.py
+++ Dynamic programming/MaximumLengthOfSubarrayWithPositiveProduct.py
@@ -43,3 +43,29 @@ class Solution:
 # Solution 2:(Efficient Solution)
 
 
+class Solution:
+    def getMaxLen(self, nums: List[int]) -> int:
+        n=len(nums)
+        pos=[0]*n
+        neg=[0]*n
+        
+        if nums[0]>0:
+            pos[0]=1
+        if nums[0]<0:
+            neg[0]=1
+        res=pos[0]
+        for i in range(1,n):
+            if(nums[i]>0):
+                pos[i]=pos[i-1]+1
+                if(neg[i-1]>0):
+                    neg[i]=neg[i-1]+1
+                else:
+                    neg[i]=0
+            elif(nums[i]<0):
+                if(neg[i-1]>0):
+                    pos[i]=neg[i-1]+1
+                else:
+                    pos[i]=0
+                neg[i]=pos[i-1]+1
+            res=max(res,pos[i])
+        return res
